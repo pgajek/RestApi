d4d6a2e5fe378e12ee588b815c894e095a1fa1f0
Patryk Gajek <pgajek94@gmail.com>
2020-11-23 12:51:28 +0100

    mongoose initialized with bodyParser

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const apiRoutes = require("./routes");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 var port = process.env.PORT || 8080;
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use("/api", apiRoutes);
+
+const dbPath = "mongodb://localhost/firstrest";
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+const mongo = mongoose.connect(dbPath, options);
+
+mongo.then(
+  () => {
+    console.log("connected");
+  },
+  (error) => {
+    console.log(error, "error");
+  }
+);
+
 app.get("/", (req, res) => {
   res.send("Hello express");
 });
-
-app.use("/api", apiRoutes);
 
 app.listen(port, function () {
   console.log("Server started at http://localhost:8080");
