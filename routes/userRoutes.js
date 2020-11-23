488150ec205beac81c90250e7bbd0d3a95a9a9e9
Patryk Gajek <pgajek94@gmail.com>
2020-11-23 16:52:48 +0100

    created user model, and first user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,22 @@
 import express from "express";
 const router = require("express").Router();
+import User from "../models/userModel";
 
-
-router.post('/signin', async (req,res)=>{
-    const signInUser = await 
-})
-
-
-
+router.post("/signin", async (req, res) => {
+  const signInUser = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (signInUser) {
+    res.send({
+      name: signInUser.name,
+      email: signInUser.email,
+      isAdmin: signInUser.isAdmin,
+      //token and id in need
+    });
+  } else {
+    res.status(401).send({ msg: "Inalig Email or password" });
+  }
+});
 
 module.exports = router;
